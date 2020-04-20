fa7569ac5fa06bc06f3627030090beec6375368e
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-20 16:54:46 +0530

    mulitple inputs using map()

---
+++ python input/mulipleinputs/map().py
@@ -0,0 +1,10 @@
+"""The syntax for map function is map (fun, iter). 
+Here fun is the function to which the map function passes each iterable."""
+a, b = map(int, input("enter the number: ").split())
+print("entered first number is: ", a)
+print("entered second number is: ", b)
+
+
+"""enter the number: 2 3
+entered first number is:  2
+entered second number is:  3"""
