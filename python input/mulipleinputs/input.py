5e1b8eaf2b037fc29f0651edff2700bbd5d713d3
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-20 16:40:05 +0530

    mulitple inputs using raw_inputs

---
+++ python input/mulipleinputs/input.py
@@ -0,0 +1,4 @@
+# mulitple input using input
+a, b = input("enter the first name: "), input("enter the second name: ")
+print("entered the first name is: ", a)
+print("entered the second name is: ", b)
