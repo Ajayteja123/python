2b7a6d813224df63182dca95a2a3277b7fd7eee1
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-20 18:45:32 +0530

    outputs in python

---
+++ pythonOutputs/outputSyntax.py
@@ -0,0 +1,23 @@
+#Syntax: print (value(s), sep= ' ', end = 'n', file = file, flush = flush)
+
+# Printing a String or Text:
+print('Hello World!')
+print("I am Learning Python on FACE Prep")
+
+
+# Printing a Value/Integer:
+num = 5
+print(num)
+
+
+# Printing a Value along with a String:
+num = 4
+print("Cats have", num, "legs")
+
+
+# Printing a String using sep
+print('hello', 'all', sep=':)')
+
+
+# Printing a String using end
+print('hello', ' all', end=' :)')
