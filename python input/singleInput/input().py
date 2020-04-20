d1ed9ab7580ec18ee7c6a4402c6b53dc1ebd0198
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-20 17:36:19 +0530

    single input

---
+++ python input/singleInput/input().py
@@ -0,0 +1,24 @@
+
+
+a = input("Enter a webiste name: ")
+print(a)
+print(type(a))
+
+b = input("Enter your lucky number: ")
+print(b)
+print(type(b))
+
+c = input("Enter a random decimal number: ")
+print(c)
+print(type(c))
+
+
+""" Enter a webiste name: www.google.com
+www.google.com
+<class 'str'>
+Enter your lucky number: 3
+3
+<class 'str'>
+Enter a random decimal number: 44.5
+44.5
+<class 'str'>"""
