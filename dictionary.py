2bcaba69990b24a2491cbbf5ca9820926914fe2f
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-28 15:50:44 +0530

    dictinory

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# userinfo = {
+#   'name': 'Ajay',
+#  'age': '19',
+# 'favorites': ['kingman']
+# }
+
+#userinfo['favorites[1]'] = 'ajay'
+# print(userinfo['favorites'])
+
+# for i in userinfo.values():
+# print(i)
+
+# delection
+# print(userinfo.pop('favorites'))
+dic = {'name': '', 'age': ''}
+#d = dic.fromkeys(['name', 'age'], 'unknown')
+# print(d)
+# print(dic['names'])
+print(dic.get('names'))
