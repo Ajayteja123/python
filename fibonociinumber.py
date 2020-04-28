78d921556881f2c7abb3cab5ecf06dc5501e16d3
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-04-28 16:10:56 +0530

    fibonciiseries

---
+++ fibonociinumber.py
@@ -0,0 +1,13 @@
+nterms = int(input("enter the number of terms"))
+n1, n2 = 0, 1
+count = 0
+if nterms <= 0:
+    print("enter the postive number")
+elif nterms == 1:
+    print("fibnnocies is 1")
+while count < nterms:
+    print(n1)
+    nth = n1+n2
+    n1 = n2
+    n2 = nth
+    count += 1
