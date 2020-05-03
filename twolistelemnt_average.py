587094bb69d040717bd1b6ce05d711833896083a
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-05-03 20:35:53 +0530

    Taking avearge of two numbers for different list using zip function by(lambda,*args)

--- twolistelemnt_average.py
+++ twolistelemnt_average.py
@@ -22,11 +22,26 @@
 
 
 # average([[1, 2, 3], [1, 4, 5], [2, 3, 5]])
-def average_finder(l1, l2):
-    average = []
-    for pair in zip(l1, l2):
-        average.append(sum(pair)/len(pair))
-    return average
+# def average_finder(l1, l2):
+#     average = []
+#     for pair in zip(l1, l2):
+#         average.append(sum(pair)/len(pair))
+#     return average
 
 
+# print(average_finder([1, 2, 3], [1, 2, 3]))
+
+
+# def average_finder(*args):
+#     average = []
+#     for pair in zip(*args):
+#         average.append(sum(pair)/len(pair))
+#     return average
+
+
+# print(average_finder([1, 2, 3], [1, 2, 3]))
+
+# using lambda
+average_finder = lambda *args: [sum(pair)/len(pair) for pair in zip(*args)]
+# return average
 print(average_finder([1, 2, 3], [1, 2, 3]))
