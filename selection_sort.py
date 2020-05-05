81208c737d6829ee7b0f9f6e886e004a0281283a
Ajayteja123 <morramshettyajayteja148@gmail.com>
2020-05-05 13:57:07 +0530

    selection sort code

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+# def selction_sort(l):
+#     for i in range(0, len(l)-1):
+#         smallest = i
+#         for j in range(i+1, len(l)):
+#             if l[j] < l[i]:
+#                 smallest = j
+#         alist[i], alist[smallest] = alist[smallest], alist[i]
+
+
+# alist = input("enter the list: ").split(',')
+# alist = [int(x) for x in alist]
+# selction_sort(alist)
+# print(f"sorted list is :{alist}")
+
+# simple way to sort a list is
+# alist = [1, 2, 6, 4, 7]
+# alist.sort(reverse=True)  # reverse=true give desending order of alist
+# print(alist)
+
